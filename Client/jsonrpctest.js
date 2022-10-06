78a27f3d469651e184a6e5a31188fa4fc4dd02f2
ngocthieuff <ptnngoc22@gmail.com>
2022-10-06 09:30:50 +0700

    add sync await

--- Client/jsonrpctest.js
+++ Client/jsonrpctest.js
@@ -1,24 +1,37 @@
 const request = require('request');
 
-// User and password specified like so: node index.js username password.
-let options = {
-    url: "http://0.0.0.0:8545",
-    method: "post",
-    headers:
-    { 
-     "content-type": "text/plain"
-    },
-    body: JSON.stringify({"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1})
-};
-console.time("execution time");
+async function execute() {
+    // User and password specified like so: node index.js username password.
+    let options = {
+        url: "http://0.0.0.0:8545",
+        method: "post",
+        headers:
+        { 
+        "content-type": "text/plain"
+        },
+        body: JSON.stringify({"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1})
+    };
+    console.time("execution time");
 
-for(i=0; i<1000; i++) {
-request(options, (error, response, body) => {
-    if (error) {
-        console.error('An error has occurred: ', error);
-    } else {
-      console.log('Post successful: body: ', body);
+    for(i=0; i<20000; i++) {
+        const response = await doRequest(options)
+        // console.log(response);
     }
-})
+    console.timeEnd("execution time");
 }
-console.timeEnd("execution time");
+
+
+function doRequest(options) {
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error)
+                console.error('An error has occurred: ', error);
+            } else {
+                resolve(body)
+            }
+        })
+    })
+}
+
+execute()
--- Client/web3test.js
+++ Client/web3test.js
@@ -1,10 +1,14 @@
 var Web3 = require('web3');
 var web3 = new Web3('http://0.0.0.0:8545');
 
+async function execute() {
 console.time("execution time");
 
-for(i=0; i<1000; i++) {
-  web3.eth.getBlockNumber()
-  .then(console.log);
+for(i=0; i<20000; i++) {
+ const resp = await web3.eth.getBlockNumber();
+ //console.log(resp);
 }
 console.timeEnd("execution time");
+}
+
+execute();
